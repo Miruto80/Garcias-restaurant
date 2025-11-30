f3202fec7d680f10f1566f8a9e664439f3698268
Miruto80 <miguelfernando8012@gmail.com>
2025-11-30 19:09:09 -0400

    Animaciones

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,20 +1,25 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from 'framer-motion';
 import Home from "../pages/Home.jsx";
 import Navbar from "../components/Navbar.jsx";
 import ChatBot from "../components/ChatBot.jsx";
 import Menu from "../pages/Menu.jsx";
 import Footer from "../components/Footer.jsx";
 import ScrollToTop from "../components/ScrollToTop.jsx";
+import FadeWrapper from "../components/FadeWrapper.jsx";
 
 function AppRoutes() {
+   const location = useLocation();
   return (
     <>
       <Navbar />
       <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Menu" element={<Menu />} />
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<FadeWrapper><Home /></FadeWrapper>} />
+        <Route path="/Menu" element={<FadeWrapper><Menu /></FadeWrapper>} />
       </Routes>
+    </AnimatePresence>
       <ChatBot />
       <div id="contacto">
       <Footer />
